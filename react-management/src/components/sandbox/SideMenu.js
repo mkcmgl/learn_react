984fcf5c23b154fb7f60fdd4da66ec5787e3716e
mkcmgl <2515318247@qq.com>
2023-02-02 17:13:09 +0800

    manegement-sideMenu-onOpenChange

--- react-management/src/components/sandbox/SideMenu.js
+++ react-management/src/components/sandbox/SideMenu.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Layout, Menu } from 'antd';
 import {
     UserOutlined,
@@ -9,64 +9,143 @@ import {
 } from '@ant-design/icons';
 import { withRouter } from 'react-router-dom'
 import './index.css'
-
+import axios from 'axios';
 
 const { Sider } = Layout;
 
-const items = [
-    {
-        key: "/home",
-        label: "首页",
-        icon: <UserOutlined />
-    },
-    {
-        key: "/user-manage",
-        label: "用户管理",
-        icon: <LaptopOutlined />,
-        children: [
-            {
-                key: "/user-manage/list",
-                label: "用户列表",
-                // icon: <UserOutlined />
-            }
-        ]
-    },
-    {
-        key: "/right-manage",
-        label: "权限管理",
-        icon: <NotificationOutlined />,
-        children: [
-            {
-                key: "/right-manage/role/list",
-                label: "角色列表",
-                // icon: <UserOutlined />
-            },
-            {
-                key: "/right-manage/right/list",
-                label: "权限列表",
-                // icon: <UserOutlined />
-            }
-        ]
-    }
-]
+// const items = [
+//     {
+//         key: "/home",
+//         label: "首页",
+//         icon: <UserOutlined />
+//     },
+//     {
+//         key: "/user-manage",
+//         label: "用户管理",
+//         icon: <LaptopOutlined />,
+//         children: [
+//             {
+//                 key: "/user-manage/list",
+//                 label: "用户列表",
+//                 // icon: <UserOutlined />
+//             }
+//         ]
+//     },
+//     {
+//         key: "/right-manage",
+//         label: "权限管理",
+//         icon: <NotificationOutlined />,
+//         children: [
+//             {
+//                 key: "/right-manage/role/list",
+//                 label: "角色列表",
+//                 // icon: <UserOutlined />
+//             },
+//             {
+//                 key: "/right-manage/right/list",
+//                 label: "权限列表",
+//                 // icon: <UserOutlined />
+//             }
+//         ]
+//     }
+// ]
 function SideMenu(props) {
+    const [menu, setMenu] = useState([])
+    useEffect(() => { 
+        axios.get("http://localhost:5000/rights?_embed=children").then((response) => {
+            console.log(response)
+        //    response.data.forEach((items) => {
+        //        items.children.forEach((children) => children.label = children.title)
+        //        items.label = items.title
+        //         if (items.id === 1) { 
+        //             items.label = items.title
+        //             items.children=''
+                    
+        //         }
+        //     })
+            setMenu(response.data)
+         })
+    },[])
+    const obj = (key, icon, label, children) => {
+        return {
+            key,
+            icon,
+            label,
+            children,
+        }
+    }
+    const iconList = {
+        "/home": <UserOutlined />,
+        "/user-manage": <UserOutlined />,
+        "/right-manage": <UserOutlined />,
 
+        "/news-manage": <UserOutlined />,
+        "/audit-manage": <UserOutlined />,
+        "/publish-manage": <UserOutlined />
+        //.......
+    }
+    const handlerMenu = (list) => {
+        const arr = []
+            list.map((item) => {
+                if (item.children && item.children.length !== 0) {
+                    return arr.push(
+                        obj(item.key, iconList[item.key], item.title, handlerMenu(item.children))
+                    )
+                } else {
+                    return (
+                        item.pagepermisson &&
+                        arr.push(obj(item.key, iconList[item.key] , item.title))
+                    )
+                }
+            })
+       
+        return arr
+    }
     const handlerClickMenu = (e) => {
         props.history.push(e.key)
     }
 
+    const [openKeys, setOpenKeys] = useState(['/home']);
+    const onOpenChange = (keys) => {
+        const latestOpenKey = keys.find((key) => { 
+            return openKeys.indexOf(key) === -1
+        });
+        // console.log(latestOpenKey)
+        // console.log(keys,'keys')
+        // console.log(menu.find(item => latestOpenKey === item.key), '11')
+        
+    //    let index= menu.findIndex(item => {
+    //         console.log(`output->`, item.key,latestOpenKey)
+    //        return item.key === latestOpenKey
+    //    })
+        
+        // console.log(menu.filter(item => latestOpenKey === item.key), '22')
+        // console.log(`output->index`, index)
+        // console.log(openKeys)
+        // setOpenKeys([latestOpenKey])
+        if (menu.findIndex(item => item.key === latestOpenKey) === -1) {
+            setOpenKeys(keys);
+        } else {
+            console.log(latestOpenKey)
+            setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+        }
+    };
     return (
         <Sider trigger={null} collapsible collapsed={false}>
+            <div style={{ display: "flex", height: "100%", "flexDirection": "column" }} >
             <div className="logo" >后台管理系统</div>
 
             <Menu
                 theme="dark"
                 mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                items={items}
-                onClick={handlerClickMenu}
-            />
+                defaultSelectedKeys={['/home']}
+                defaultOpenKeys={['/home']}
+                items={handlerMenu(menu)}
+                    onClick={handlerClickMenu}
+                    openKeys={openKeys}
+                    onOpenChange={onOpenChange}
+                />
+                </div>
         </Sider>
     )
 }
--- react-management/src/views/sandbox/home/Home.js
+++ react-management/src/views/sandbox/home/Home.js
@@ -1,7 +1,45 @@
 import React from 'react'
-
+import axios from 'axios'
+import { Button } from 'antd';
 export default function Home() {
+          // 取数据  get
+  const ajax = () => {
+    // axios.get("http://localhost:8000/posts/2").then(res => {
+    //   console.log(res.data)
+    // })
+            // 增  post
+        // axios.post("http://localhost:8000/posts",{
+        //     title:"33333",
+        //     author:"xiaoming"
+        // })
+
+        // 更新 put
+
+        // axios.put("http://localhost:8000/posts/1",{
+        //     title:"1111-修改"
+        // })
+
+        // 更新 patch
+        // axios.patch("http://localhost:8000/posts/1",{
+        //     title:"1111-修改-11111"
+        // }) 
+
+        // 删除  delete
+        // axios.delete("http://localhost:8000/posts/1")
+    
+        // _embed
+        // axios.get("http://localhost:8000/names?_embed=comments").then(res=>{
+        //     console.log(res.data)
+        // })
+
+        // _expand
+    // axios.get("http://localhost:8000/comments?_expand=name").then(res=>{
+    //         console.log(res.data)
+    //     })
+  }
   return (
-    <div>Home</div>
+    <div>
+      <Button type="primary" onClick={ajax}>Button</Button>
+    </div>
   )
 }
