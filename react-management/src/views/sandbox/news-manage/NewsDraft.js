4389f9340698ad43f87e896ff1b037bd28a46284
mkcmgl <2515318247@qq.com>
2023-02-08 15:04:03 +0800

    draft

--- react-management/src/views/sandbox/news-manage/NewsDraft.js
+++ react-management/src/views/sandbox/news-manage/NewsDraft.js
@@ -1,7 +1,31 @@
-import React from 'react'
-
+import React, { useState,useEffect} from 'react'
+import { Button, Table, Modal, notification } from 'antd'
+import axios from 'axios'
 export default function NewsDraft() {
+  const [ dataSource, setDataSource ] = useState([])
+  const { username } = JSON.parse(localStorage.getItem("token"))
+  useEffect(() => { 
+    axios.get(`/news?author=${username}&auditState=0&_expand=category`).then((response) => {
+      console.log(`output->response`, response)
+      const list = response.data
+      setDataSource(list)
+     })
+  }, [username])
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: "id",
+      render: (id) => { 
+        return <b>{ id}</b>
+      }
+    }, {
+      
+    }
+  ]
   return (
-    <div>NewsDraft</div>
+    <div>
+      <Table dataSource={dataSource} columns={columns} />
+    </div>
   )
 }
