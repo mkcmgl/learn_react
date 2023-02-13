ea677ce81c75d58e9c561e571c030a2cfdd250d8
mkcmgl <2515318247@qq.com>
2023-02-13 08:32:58 +0800

    preview new

---
+++ react-management/src/views/sandbox/news-manage/NewsPreview.js
@@ -0,0 +1,10 @@
+import React, {useEffect } from 'react'
+
+export default function NewsPreview(props) {
+  useEffect(() => { 
+    console.log(`output`)
+  })
+  return (
+    <div>NewsPreview</div>
+  )
+}
