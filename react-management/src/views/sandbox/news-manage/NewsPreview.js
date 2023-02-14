1daedaa91b47f8df1d2e9db935104a6d1d06cec6
mkcmgl <2515318247@qq.com>
2023-02-14 17:19:16 +0800

    moment

--- react-management/src/views/sandbox/news-manage/NewsPreview.js
+++ react-management/src/views/sandbox/news-manage/NewsPreview.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { PageHeader, Descriptions } from "antd"
+import { Descriptions, Card } from "antd"
 import axios from 'axios'
 import moment from 'moment'
 export default function NewsPreview(props) {
-  const [newsInfo,setNewsInfo] = useState(null)
-  useEffect(() => { 
+  const [newsInfo, setNewsInfo] = useState(null)
+  useEffect(() => {
     axios.get(`/news/${props.match.params.id}?_expand=category&_expand=role`).then(res => {
       setNewsInfo(res.data)
+
     })
   }, [props.match.params.id])
   const auditList = ["未审核", '审核中', '已通过', '未通过']
@@ -14,31 +15,28 @@ export default function NewsPreview(props) {
   return (
     <div>
 
-      newsInfo && <div>
-      <PageHeader
-        onBack={() => window.history.back()}
-        title={newsInfo.title}
-        subTitle={newsInfo.category.title}
-      >
-        <Descriptions size="small" column={3}>
-          <Descriptions.Item label="创建者">{newsInfo.author}</Descriptions.Item>
-          <Descriptions.Item label="创建时间">{moment(newsInfo.createTime).format("YYYY/MM/DD HH:mm:ss")}</Descriptions.Item>
-          <Descriptions.Item label="发布时间">{
-            newsInfo.publishTime ? moment(newsInfo.publishTime).format("YYYY/MM/DD HH:mm:ss") : "-"
-          }</Descriptions.Item>
-          <Descriptions.Item label="区域">{newsInfo.region}</Descriptions.Item>
-          <Descriptions.Item label="审核状态" ><span style={{ color: "red" }}>{auditList[newsInfo.auditState]}</span></Descriptions.Item>
-          <Descriptions.Item label="发布状态" ><span style={{ color: "red" }}>{publishList[newsInfo.publishState]}</span></Descriptions.Item>
-          <Descriptions.Item label="访问数量">{newsInfo.view}</Descriptions.Item>
-          <Descriptions.Item label="点赞数量">{newsInfo.star}</Descriptions.Item>
-          <Descriptions.Item label="评论数量">0</Descriptions.Item>
+      {
+        newsInfo && <div>
+    
+          <Descriptions title={newsInfo.title} size="small" column={3}>
+            <Descriptions.Item label="创建者">{newsInfo.author}</Descriptions.Item>
+            <Descriptions.Item label="创建时间">{moment(newsInfo.createTime).format("YYYY/MM/DD HH:mm:ss")}</Descriptions.Item>
+            <Descriptions.Item label="发布时间">{
+              newsInfo.publishTime ? moment(newsInfo.publishTime).format("YYYY/MM/DD HH:mm:ss") : "-"
+            }</Descriptions.Item>
+            <Descriptions.Item label="区域">{newsInfo.region}</Descriptions.Item>
+            <Descriptions.Item label="审核状态" ><span style={{ color: "red" }}>{auditList[newsInfo.auditState]}</span></Descriptions.Item>
+            <Descriptions.Item label="发布状态" ><span style={{ color: "red" }}>{publishList[newsInfo.publishState]}</span></Descriptions.Item>
+            <Descriptions.Item label="访问数量">{newsInfo.view}</Descriptions.Item>
+            <Descriptions.Item label="点赞数量">{newsInfo.star}</Descriptions.Item>
+            <Descriptions.Item label="评论数量">0</Descriptions.Item>
 
-        </Descriptions>
-      </PageHeader>
-        <div dangerouslysetinnerhtml={{ __html: newsInfo.content }}
-          style={{ margin: "0 24px", border: "1px  solid gray"  }}>
+          </Descriptions>
+          <div dangerouslySetInnerHTML={{ __html: newsInfo.content }}
+            style={{ margin: "0 24px", border: "1px  solid gray" ,padding:"5px" }}>
+          </div>
         </div>
-        </div>
+      }
     </div>
   )
 }
