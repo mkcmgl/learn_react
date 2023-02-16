5aab7e817b98fa1d2128e1fbc6ddaf77228e8b5f
mkcmgl <2515318247@qq.com>
2023-02-16 08:49:52 +0800

    audit list

--- react-management/src/views/sandbox/publish-manage/Sunset.js
+++ react-management/src/views/sandbox/publish-manage/Sunset.js
@@ -1,7 +1,19 @@
-import React from 'react'
 
+import React from 'react'
+import NewsPublish from '../../../components/publish-manage/NewsPublish'
+import usePublish from '../../../components/publish-manage/usePublish'
+import { Button } from 'antd'
 export default function Sunset() {
+  const { dataSource, handleSunset } = usePublish(3)
   return (
-    <div>Sunset</div>
+    <div>
+      <NewsPublish dataSource={dataSource} button={(id) =>
+        <Button danger onClick={() => handleSunset(id)} >
+          删除
+        </Button>}>
+
+      </NewsPublish>
+    </div>
   )
 }
+
--- react-management/src/views/sandbox/publish-manage/Unpublished.js
+++ react-management/src/views/sandbox/publish-manage/Unpublished.js
@@ -1,7 +1,19 @@
-import React from 'react'
 
+import React from 'react'
+import NewsPublish from '../../../components/publish-manage/NewsPublish'
+import usePublish from '../../../components/publish-manage/usePublish'
+import { Button } from 'antd'
 export default function Unpublished() {
+  const { dataSource, handleSunset } = usePublish(1)
   return (
-    <div>Unpublished</div>
+    <div>
+      <NewsPublish dataSource={dataSource} button={(id) =>
+        <Button danger onClick={() => handleSunset(id)} >
+          发布
+        </Button>}>
+
+      </NewsPublish>
+    </div>
   )
 }
+
