e639a0efc452da5bc4f78484947d10d4211b245a
mkcmgl <2515318247@qq.com>
2023-02-17 11:24:06 +0800

    store

--- react-management/src/redux/store.js
+++ react-management/src/redux/store.js
@@ -2,15 +2,20 @@ import { legacy_createStore as createStore,combineReducers} from "redux"
 // 解决 createStore有删除线问题：
 // import {legacy_createStore as createStore  } from "redux";
 import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { CollApsedReducer } from './reducers/CollapsedReducer'
+import { LoadingReducer } from "./reducers/LoadingReducer";
 
 
-const persistConfig = { 
+const persistConfig = {
+    key: 'mgl',
+    storage,
+    blacklist: ['LoadingReducer']
+}
 
- }
-
-const { reducer } = combineReducers({
-    CollApsedReducer
+const  reducer  = combineReducers({
+    CollApsedReducer,
+    LoadingReducer
 })
 
 const persistedReducer = persistReducer(persistConfig, reducer)
